9f7c1768543f7d7911504c3654f80deec31a38c4
JuanPieceOficial <vivenesjuan3320@gmail.com>
2026-02-27 03:47:16 +0000

    chore: remove ABI splits from Gradle, use flutter split flag

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,15 +48,6 @@ android {
         }
     }
 
-    // split per ABI produces separate binaries for each architecture
-    splits {
-        abi {
-            isEnable = true
-            reset()
-            include("armeabi-v7a", "arm64-v8a", "x86_64")
-            isUniversalApk = false
-        }
-    }
 }
 
 flutter {
