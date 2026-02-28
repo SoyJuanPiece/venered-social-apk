166817a3b7a20a343782cac3342f10aa6527554c
juanpiece2025 <vivenesjuan332@gmail.com>
2026-02-28 19:18:40 +0000

    fix: add missing dart:async import for StreamSubscription

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
