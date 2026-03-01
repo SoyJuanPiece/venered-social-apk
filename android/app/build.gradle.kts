5f8a5e8053ff0b5bfff1d7e0aa13c198b505d432
vivenesjuan332 <vivenesjuan332@gmail.com>
2026-03-01 05:37:38 +0000

    fix: corregir sintaxis de build.gradle y namespace v1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+
 android {
-    namespace = "com.example.venered_social"
+    namespace = "com.juanpiece.venered"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -14,19 +14,12 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 
-        // Enable core library desugaring for libraries that require newer Java APIs
-        isCoreLibraryDesugaringEnabled = true
-    }
-
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "1.8"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.juanpiece.venered"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -35,11 +28,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
-
-            // enable code shrinking and resource shrinking for smaller APKs
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
@@ -48,7 +37,6 @@ android {
             )
         }
     }
-
 }
 
 flutter {
@@ -57,7 +45,6 @@ flutter {
 
 dependencies {
     implementation("com.google.android.material:material:1.10.0")
-    // Use a known compatible version available on Maven Central / Google
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.2.2")
 }
 
