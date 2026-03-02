fb03c194ee815707c10da9b6fefde5db29f8604f
vivenesjuan332 <vivenesjuan332@gmail.com>
2026-03-02 03:06:18 +0000

    fix: inject legacy 'flutter' property into subprojects to fix record_android compatibility

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -13,6 +13,11 @@ allprojects {
         google()
         mavenCentral()
     }
+    
+    // Forzar versiones globales para todos los plugins (incluyendo record_android)
+    project.extra.set("flutter.compileSdkVersion", 35)
+    project.extra.set("flutter.targetSdkVersion", 35)
+    project.extra.set("flutter.minSdkVersion", 23)
 }
 
 val newBuildDir: Directory =
@@ -25,12 +30,21 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
     
-    // Fix para plugins que no definen compileSdk (como record_android)
+    // PARCHE DEFINITIVO: Inyectar propiedad 'flutter' en la extensión 'android' de los subproyectos
     afterEvaluate {
         if (project.hasProperty("android")) {
             val android = project.extensions.getByName("android")
             if (android is com.android.build.gradle.BaseExtension) {
                 android.compileSdkVersion(35)
+                
+                // Si el plugin busca 'android.flutter', se lo proporcionamos
+                if (!android.hasProperty("flutter")) {
+                    (android as ExtensionAware).extensions.add("flutter", mapOf(
+                        "compileSdkVersion" to 35,
+                        "targetSdkVersion" to 35,
+                        "minSdkVersion" to 23
+                    ))
+                }
             }
         }
     }
