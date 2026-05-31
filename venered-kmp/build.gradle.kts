ea4351b7eceb7b7498f2282f1be2d410b322a0d3
Juan Carlos Vivenes <vivenesjuan332@gmail.com>
2026-05-30 22:46:30 -0400

    fix(kmp): remove duplicate repository declarations to fix CI build conflict

--- venered-kmp/build.gradle.kts
+++ venered-kmp/build.gradle.kts
@@ -4,10 +4,3 @@ plugins {
     id("com.android.application") version "8.1.4" apply false
     id("com.android.library") version "8.1.4" apply false
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
