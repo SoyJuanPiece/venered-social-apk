6be6aed27bccf88d3efad3c3226360d1d6a562aa
Juan Carlos Vivenes <vivenesjuan332@gmail.com>
2026-05-30 23:37:18 -0400

    ci: fix gradle wrapper generation and add google-services placeholder

--- venered-kmp/build.gradle.kts
+++ venered-kmp/build.gradle.kts
@@ -3,11 +3,5 @@ plugins {
     kotlin("android") version "1.9.20" apply false
     id("com.android.application") version "8.1.4" apply false
     id("com.android.library") version "8.1.4" apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
