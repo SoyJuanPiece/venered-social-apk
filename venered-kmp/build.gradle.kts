4b90a5d6502f5ae7ae5b8bba0abec6d2fe9ffab7
Juan Carlos Vivenes <vivenesjuan332@gmail.com>
2026-05-30 23:02:37 -0400

    fix(ci): restore original build files and update settings to allow project repositories

--- venered-kmp/build.gradle.kts
+++ venered-kmp/build.gradle.kts
@@ -4,3 +4,10 @@ plugins {
     id("com.android.application") version "8.1.4" apply false
     id("com.android.library") version "8.1.4" apply false
 }
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
